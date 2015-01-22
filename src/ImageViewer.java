31373ed12122233da3452820e48948a64d877ed6
timzijl <timzijl@hotmail.com>
2015-01-22 13:06:36 +0100

    revert shit

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -34,6 +34,8 @@ public class ImageViewer {
 		toolbar.add(stepOne);
 		JButton stepHundred = new JButton("Step 100");
 		toolbar.add(stepHundred);
+		JButton Simulatie = new JButton("Sumulatie");
+		toolbar.add(Simulatie);
 		contentPane.add(toolbar, BorderLayout.WEST);
 		
 		JLabel label = new JLabel("I am a label.");
