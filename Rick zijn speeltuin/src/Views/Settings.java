d4e203cdf37c16b2b1950440f86d065c8c1b54a7
tvveen <t.van.veen@st.hanze.nl>
2015-02-02 22:12:58 +0100

    settings wolfpanel

--- Rick zijn speeltuin/src/Views/Settings.java
+++ Rick zijn speeltuin/src/Views/Settings.java
@@ -13,10 +13,11 @@ public class Settings {
 	
 	private JPanel panel;
 	
-	private String[] list = {"Fox", "Rabbit", "Grass"};
+	private String[] list = {"Wolf", "Fox", "Rabbit", "Grass"};
 	
 	public Settings()
 	{
+		WolfPanel wolfpanel = new WolfPanel();
 		FoxPanel foxpanel = new FoxPanel();
 		RabbitPanel rabbitpanel = new RabbitPanel();
 		GrassPanel grasspanel = new GrassPanel();
@@ -28,11 +29,12 @@ public class Settings {
 		JComboBox<String> dropList = new JComboBox<String>(list);
 		dropList.setBounds(1, 1, 500, 40);
 		
+		final JPanel wolfPanel = wolfpanel.getWolfPanel();
 		final JPanel foxPanel = foxpanel.getFoxPanel();
 		final JPanel rabbitPanel = rabbitpanel.getRabbitPanel();
 		final JPanel grassPanel = grasspanel.getGrassPanel();
 				
-			
+		panel.add(wolfPanel);
 		panel.add(foxPanel);
 		panel.add(rabbitPanel);
 		panel.add(grassPanel);	
@@ -46,15 +48,24 @@ public class Settings {
 				String selected = ((JComboBox<String>) itemEvent.getSource()).getSelectedItem().toString();
 				//System.out.println(((JComboBox<String>) itemEvent.getSource()).getSelectedItem());
 				
+				if(selected == "Wolf")
+				{		
+					wolfPanel.setVisible(true);
+					foxPanel.setVisible(false);
+					rabbitPanel.setVisible(false);
+					grassPanel.setVisible(false);
+				}
 				if(selected == "Fox")
 				{					
 					foxPanel.setVisible(true);
+					wolfPanel.setVisible(false);
 					rabbitPanel.setVisible(false);
 					grassPanel.setVisible(false);
 				}
 				else if(selected== "Rabbit")
 				{
 					rabbitPanel.setVisible(true);
+					wolfPanel.setVisible(false);
 					foxPanel.setVisible(false);
 					grassPanel.setVisible(false);
 					
@@ -62,6 +73,7 @@ public class Settings {
 				else if(selected == "Grass")
 				{
 					grassPanel.setVisible(true);
+					wolfPanel.setVisible(false);
 					foxPanel.setVisible(false);
 					rabbitPanel.setVisible(false);
 				}
